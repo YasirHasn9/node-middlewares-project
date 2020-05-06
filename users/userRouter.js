b2d437b77ca7390f9999675975d66dc97a5fc4bf
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-06 15:37:13 -0700

    validateUserId

--- users/userRouter.js
+++ users/userRouter.js
@@ -23,79 +23,46 @@ router.post("/", async (req, res, next) => {
     next(err);
   }
 });
-router.get("/:id", async (req, res, next) => {
-  try {
-    const user = await Users.getById(req.params.id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: "Not Found" });
-    }
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
+router.get("/:id", validateUserId(), (req, res) => {
+  res.json(req.user);
 });
 
-router.get("/:id/posts", async (req, res, next) => {
+router.get("/:id/posts", validateUserId(), async (req, res, next) => {
   // do your magic!
   try {
-    const user = await Users.getById(req.params.id);
-    if (user) {
-      const posts = await Users.getUserPosts(req.params.id);
-      res.json(posts);
-    } else {
-      res.status(404).json({ message: "Not Found" });
-    }
+    const posts = await Users.getUserPosts(req.params.id);
+    res.json(posts);
   } catch (err) {
     console.log("GET: :/posts", err);
     next(err);
   }
 });
 
-router.post("/:id/posts", async (req, res, next) => {
+router.post("/:id/posts", validateUserId(), async (req, res, next) => {
   // do your magic!
   try {
-    // check the user
-    const user = await Users.getById(req.params.id);
-    if (user) {
-      const newPost = await Posts.insert(req.body);
-      return res.json(newPost);
-    } else {
-      res.status(404).json({ message: "Not Found" });
-    }
+    const newPost = await Posts.insert(req.body);
+    return res.json(newPost);
   } catch (err) {
     next(err);
   }
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", validateUserId(), async (req, res) => {
   // do your magic!
   try {
-    // check the user
-    const user = await Users.getById(req.params.id);
-    if (user) {
-      await Users.remove(req.params.id);
-      res.status(202).end();
-    } else {
-      res.status(404).json({ message: "Not Found" });
-    }
+    await Users.remove(req.params.id);
+    res.status(202).end();
   } catch (err) {
     next(err);
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", validateUserId(), async (req, res) => {
   // do your magic!
   try {
-    // check the user
-    const user = await Users.getById(req.params.id);
-    if (user) {
-      const updatedUser = await Users.update(req.params.id, req.body);
-      res.json(updatedUser);
-    } else {
-      res.status(404).json({ message: "Not Found" });
-    }
+    const updatedUser = await Users.update(req.params.id, req.body);
+    res.json(updatedUser);
   } catch (err) {
     next(err);
   }
@@ -103,8 +70,22 @@ router.put("/:id", async (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId() {
   // do your magic!
+  return async (req, res, next) => {
+    try {
+      let user = await Users.getById(req.params.id);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(404).json({ message: "Not Found" });
+      }
+    } catch (err) {
+      console.lor(err);
+      next(err);
+    }
+  };
 }
 
 function validateUser(req, res, next) {
