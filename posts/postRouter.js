3ef85bdb359208793cc49993a8fc40af9f56e6e9
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-06 21:31:09 -0700

    functions : delete and update a post

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -16,22 +16,52 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   // do your magic!
   try {
-    if (req.params.id) {
+    if (!req.params.id) {
       return res.status(404).json({ message: "Not found" });
     }
     const post = await Posts.getById(req.params.id);
-    res.json(post);
+    if (post) {
+      res.json(post);
+      next();
+    } else {
+      res.status(404).json({ message: "Post not Found" });
+    }
   } catch (err) {
     next(err);
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res, next) => {
   // do your magic!
+  try {
+    if (!req.params.id) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    await Posts.remove(req.params.id);
+    res.end();
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res, next) => {
   // do your magic!
+  try {
+    if (!req.params.id) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    const post = await Posts.getById(req.params.id);
+    if (post) {
+      const updatedPost = await Posts.update(req.params.id, req.body);
+      res.status(201).json(updatedPost);
+      next();
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 // custom middleware
